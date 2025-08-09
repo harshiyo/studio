a30b2eb72a980d29fcb941f204f81ab54ee39e24
Harsh <sutharharsh99@gmail.com>
2025-08-09 17:43:09 +0000

    container size should be following
    20ft Used (WWT)
    20ft Used (Cargo)
    2

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,16 @@
-export const containerSizes = ["Small", "Medium", "Large"] as const;
+export const containerSizes = [
+  "20ft Used (WWT)",
+  "20ft Used (Cargo)",
+  "20ft New",
+  "40ft Used (WWT)",
+  "40ft Used (Cargo)",
+  "40ft New",
+  "40ft HC Used (WWT)",
+  "40ft HC Used (Cargo)",
+  "40ft HC New",
+] as const;
 
-export type ContainerSize = typeof containerSizes[number];
+export type ContainerSize = (typeof containerSizes)[number];
 
 export type Order = {
   id: string;
@@ -9,5 +19,5 @@ export type Order = {
   containerSize: ContainerSize;
   quantity: number;
   deliveryDate: string; // ISO 8601 string format
-  status: 'pending' | 'completed';
+  status: "pending" | "completed";
 };
